c3319cb0a35f81c9abd82ca1b47d80bab87af782
Adam <amoore@uat.edu>
2021-02-11 13:24:56 -0700

    Obstacle Avoidance Implemented.

---
+++ Assets/Scripts/SampleAIController3.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TankData))]
+[RequireComponent(typeof(TankShooter))]
+[RequireComponent(typeof(TankMotor))]
+public class SampleAIController3 : MonoBehaviour
+{
+    private TankData data;
+    private TankMotor motor;
+    private TankShooter shooter;
+
+    public enum AttackState { Chase };
+    public AttackState attackState = AttackState.Chase;
+
+    public enum AvoidanceStage { NotAvoiding, ObstacleDetected, AvoidingObstacle };
+    public AvoidanceStage avoidanceStage = AvoidanceStage.NotAvoiding;
+
+    public float avoidanceTime = 2f;
+    private float exitTime;
+    public float closeEnough = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GetComponent<TankData>();
+        motor = GetComponent<TankMotor>();
+        shooter = GetComponent<TankShooter>();
+    }
+
+    private void Update()
+    {
+        if (attackState == AttackState.Chase)
+        {
+            if (avoidanceStage != AvoidanceStage.NotAvoiding)
+            {
+                Avoid();
+            }
+            else
+            {
+                Chase(GameManager.Instance.Players[0]);
+            }
+        }
+    }
+
+    public void Chase(GameObject target)
+    {
+        if (motor.RotateTowards(target.transform.position, data.turnSpeed))
+        {
+            // Do nothing
+        }
+        else if (!CanMove(data.moveSpeed))
+        {
+            avoidanceStage = AvoidanceStage.ObstacleDetected;
+        }
+        else
+        {
+            if (Vector3.SqrMagnitude(transform.position - target.transform.position) >= (closeEnough * closeEnough))
+            {
+                motor.Move(data.moveSpeed);
+            }
+            // shooter.Shoot();
+        }
+    }
+
+    public void Avoid()
+    {
+        if (avoidanceStage == AvoidanceStage.ObstacleDetected)
+        {
+            // Rotate left
+            motor.Rotate(-1 * data.turnSpeed);
+
+            // If I can now move forward, move to stage 2!
+            if (CanMove(data.moveSpeed))
+            {
+                avoidanceStage = AvoidanceStage.AvoidingObstacle;
+
+                // Set the number of seconds we will stay in Stage2
+                exitTime = avoidanceTime;
+            }
+        }
+        else if (avoidanceStage == AvoidanceStage.AvoidingObstacle)
+        {
+            // if we can move forward, do so
+            if (CanMove(data.moveSpeed))
+            {
+                // Subtract from our timer and move
+                exitTime -= Time.deltaTime;
+                motor.Move(data.moveSpeed);
+
+                // If we have moved long enough, return to chase mode
+                if (exitTime <= 0)
+                {
+                    avoidanceStage = AvoidanceStage.NotAvoiding;
+                }
+            }
+            else
+            {
+                avoidanceStage = AvoidanceStage.ObstacleDetected;
+            }
+        }
+    }
+
+    bool CanMove(float speed)
+    {
+        // Cast a ray forward in the distance that we sent in
+        RaycastHit hit;
+
+        // If our raycast hit something...
+        if (Physics.Raycast(transform.position, transform.forward, out hit, speed))
+        {
+            // ... and if what we hit is not the player...
+            if (!hit.collider.CompareTag("Player"))
+            {
+                // ... then we can't move
+                return false;
+            }
+        }
+        // otherwise, we can move, so return true
+        return true;
+    }
+}
